3fa2143d5799d853d589e42600596bacc2cee7df
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-02-18 02:12:27 -0300

    Iniciando com funções

---
+++ functions.py
@@ -0,0 +1,13 @@
+def call_numbers():
+    for number in range(0, 10):
+        print(number)
+
+
+def call_numbers_with_limit(limit):
+    list = range(0, 10)
+    # for number in    list[limit]:
+    for number in range(limit):
+        print(number)
+
+
+call_numbers_with_limit(50)
