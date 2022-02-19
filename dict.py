088c30c817f0f61e4617c2062956b6245d716ab0
Luiz Felipe Ferreira de Moraes <lffm1994@gmail.com>
2022-02-18 21:54:58 -0300

    Trabalhando com dicionários

---
+++ dict.py
@@ -0,0 +1,23 @@
+cars = {}
+cars['corola'] = "red"
+cars['fit'] = "green"
+cars['320'] = "black"
+
+print(cars)
+print(cars.keys())
+# dict_keys(['corola','fit','320'])
+cars.values()
+# dict_values('red','green','black')
+cars['corola']
+
+people = dict(Wesley='Father', Mariana='Mother', Sarah='Baby')
+print(people)
+
+if 'Wesley' in people:
+    print(people['Wesley'])
+
+family = {
+    'Wesley':'Father'
+}
+
+print(family)
